53c55f7e5b1c442a7d0e922a1c8dc657144333c5
James-May-Coding <canbailey243@aol.com>
2025-11-05 00:08:40 -0500

    .

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseAnon = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  process.env.VITE_SUPABASE_URL,
+  process.env.VITE_SUPABASE_ANON_KEY
 );
 
 export const supabaseService = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
+  process.env.VITE_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
