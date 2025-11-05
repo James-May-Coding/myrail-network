b4134777a77822d808d4db9450acca8453fe3f26
James-May-Coding <canbailey243@aol.com>
2025-11-05 00:18:12 -0500

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = "https://dmjazdpluclinainckit.supabase.co";
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
