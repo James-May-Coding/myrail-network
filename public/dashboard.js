1c25cc8777a4ae2d2a98c80714bd0b1fd47d4700
James-May-Coding <canbailey243@aol.com>
2025-11-05 00:21:11 -0500

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = "https://dmjazdpluclinainckit.supabase.co";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtamF6ZHBsdWNsaW5haW5ja2l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyOTYyNDEsImV4cCI6MjA3Nzg3MjI0MX0.BtKwm3wds62gOrabC5lY4561zawTdT_f-o9_frO2TRk";
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 let currentUser = null;
