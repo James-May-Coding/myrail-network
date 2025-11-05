8e2ce5676b1316007db0dd39238b16c72797833c
James-May-Coding <canbailey243@aol.com>
2025-11-04 22:24:08 -0500

    Update discord.js

--- api/auth/discord.js
+++ api/auth/discord.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     const clientId = process.env.DISCORD_CLIENT_ID;
     const redirect = encodeURIComponent(process.env.DISCORD_REDIRECT_URI);
     const scope = encodeURIComponent('identify email');
-    const oauthUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirect}&response_type=code&scope=${scope}`;
+    const oauthUrl = `https://discord.com/oauth2/authorize?client_id=1435440980210876520&response_type=code&redirect_uri=https%3A%2F%2Fmyrail-network.vercel.app%2Fapi%2Fauth%2Fcallback.js&scope=identify+email`;
   
     res.writeHead(302, { Location: oauthUrl });
     res.end();
